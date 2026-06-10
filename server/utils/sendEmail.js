c6d68d2e86e81357e7dcd4894b3ba11e3a5ae3d3
Wirngo Elias <wirngoelias@gmail.com>
2026-06-10 11:05:15 +0100

    changes made to package-lock.json package.json and sendemail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,49 +1,26 @@
-const dns = require("dns");
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: parseInt(process.env.EMAIL_PORT, 10) || 587,
-  secure: process.env.EMAIL_SECURE === "true" || false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  requireTLS: true,
-  tls: {
-    rejectUnauthorized: false,
-  },
-  lookup: (hostname, options, callback) => {
-    return dns.lookup(hostname, { family: 4, all: false }, callback);
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
-transporter.verify()
-  .then(() => {
-    console.log("Email transporter verified");
-  })
-  .catch((err) => {
-    console.error("Email transporter verification failed:", err);
-  });
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
   try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    const msg = {
       to,
+      from: process.env.EMAIL_SENDER,
       subject,
       text,
-    });
-    console.log("Email sent:", info.messageId);
-    return info;
+    };
+
+    const [response] = await sgMail.send(msg);
+    console.log("Email sent:", response.statusCode, response.headers);
+    return response;
   } catch (error) {
     console.error("Failed sending email:", error);
+    if (error.response) {
+      console.error("SendGrid response body:", error.response.body);
+    }
     throw error;
   }
 };
 
-console.log("Email user:", process.env.EMAIL_USER);
 module.exports = sendEmail;
